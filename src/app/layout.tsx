509a1769cd117b2d1cc2937fb6e7e6397088ee7f
Madson Santos <madsonsamsung@gmail.com>
2024-12-14 22:33:27 -0300

    install and configure the shadcn ui library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-export const metadata: Metadata = {
-  title: "fleet-control-front",
-};
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -12,8 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body>
+    <html lang="pt-br" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <Toaster position="top-center" />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/deliveries");
-
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      loading...
-    </div>
-  );
+  redirect("/delivery");
 }
